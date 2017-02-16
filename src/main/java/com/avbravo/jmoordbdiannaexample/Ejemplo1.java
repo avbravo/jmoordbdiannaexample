6b635aefa3176ee5c4982d952ba43ed142f028a7
avbravo <avbravo@PC02-UTICA>
2017-02-16 14:19:29 -0500

    estructura basica

---
+++ src/main/java/com/avbravo/jmoordbdiannaexample/Ejemplo1.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.jmoordbdiannaexample;
+
+
+import java.util.List;
+import java.util.Optional;
+import org.jnosql.diana.api.document.Document;
+import org.jnosql.diana.api.document.DocumentCollectionManager;
+import org.jnosql.diana.api.document.DocumentCollectionManagerFactory;
+import org.jnosql.diana.api.document.DocumentCondition;
+import org.jnosql.diana.api.document.DocumentConfiguration;
+import org.jnosql.diana.api.document.DocumentEntity;
+import org.jnosql.diana.api.document.DocumentQuery;
+import org.jnosql.diana.api.document.Documents;
+import org.jnosql.diana.mongodb.document.MongoDBDocumentConfiguration;
+
+/**
+ *
+ * @author avbravo
+ */
+public class Ejemplo1 {
+
+    public static final String DATABASE = "tallerdb";
+    public static final String DOCUMENT_COLLECTION = "person";
+
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+    public void ejemplo(){
+        // TODO code application logic here
+        DocumentConfiguration configuration = new MongoDBDocumentConfiguration();
+        try (DocumentCollectionManagerFactory collectionFactory = configuration.get();) {
+            DocumentCollectionManager collectionManager = collectionFactory.get(DATABASE);
+
+            DocumentEntity entity = DocumentEntity.of(DOCUMENT_COLLECTION);
+            entity.add(Document.of("name", "Daniel Soro"));
+            entity.add(Document.of("age", 26));
+
+            DocumentEntity entitySaved = collectionManager.save(entity);
+            Optional<Document> id = entitySaved.find("_id");
+
+            DocumentQuery query = DocumentQuery.of(DOCUMENT_COLLECTION);
+            query.and(DocumentCondition.eq(id.get()));
+            List<DocumentEntity> documentsFound = collectionManager.find(query);
+            for (DocumentEntity de : documentsFound) {
+                System.out.println(de.getDocuments());
+                for (Document d : de.getDocuments()) {
+                    System.out.println("d " + d.toString());
+
+                }
+            }
+
+        }
+        //catch(Exception ex){
+//            System.out.println("error() "+ex.getLocalizedMessage());
+//        }
+
+    }
+
+}
